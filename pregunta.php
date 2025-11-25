341c8f3584ff013068ac44c47e4637643ba6ef4e
Maksym Derezyuk <100006117.joan23@fje.edu>
2025-11-25 17:48:49 +0100

    funcionalidad de respuesta + estilo en archivos

--- pregunta.php
+++ pregunta.php
@@ -45,9 +45,21 @@ $respuestas = $stmtResp->fetchAll(PDO::FETCH_ASSOC);
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-
-    <?php require 'includes/header.php'; // Asegúrate de que este archivo existe, si no copia el nav del index ?>
-
+    <header>
+        <nav>
+            <a href="index.php" class="logo">Foro</a>
+            <ul class="nav-links">
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="crear_pregunta.php">Nueva Pregunta</a></li>
+                <li><a href="perfil.php">Perfil</a></li>
+                <?php if(isset($_SESSION['usuario'])): ?>
+                    <li><a href="actions/logout.php">Cerrar Sesión</a></li>
+                <?php else: ?>
+                    <li><a href="login.php">Iniciar Sesión</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </header>
     <div class="container">
         
         <div class="card" style="border-left: 4px solid var(--color-orange); margin-top: 2rem;">
